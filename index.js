6b5b0bbebb6678f24c71a12bddf2f608e9145bb6
Mike Starr <imhs@protonmail.com>
2022-01-13 07:43:01 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+
+function superbowlWin(arr) {
+  const results = arr.find((el) => el.result === "W");
+  console.log(results);
+  if (results) {
+    return results.year;
+  }
+}
